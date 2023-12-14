b168ed14cefe2b3462cd3dbf8fe6b7b6060c5457
Coder731 <stephenobrien1@gmail.com>
2023-12-14 13:45:58 +0000

    Add code to main py
    from
    
    nltk org

--- main.py
+++ main.py
@@ -9,4 +9,30 @@ entities = nltk.chunk.ne_chunk(tagged)
 # print(entities)
 from nltk.corpus import treebank
 t = treebank.parsed_sents('wsj_0001.mrg')[0]
-t.draw()
+# t.draw()
+from nltk.tokenize.treebank import TreebankWordTokenizer, TreebankWordDetokenizer
+s = '''Good muffins cost $3.88\nin New York.  Please buy me\ntwo of them.\nThanks.'''
+d = TreebankWordDetokenizer()
+t = TreebankWordTokenizer()
+toks = t.tokenize(s)
+d.detokenize(toks)
+expected_tokens = ['Good', 'muffins', 'cost', '$', '3.88', 'in', 'New', '-LRB-', 'York', '-RRB-', '.', 'Please', '-LRB-', 'buy', '-RRB-', 'me', 'two', 'of', 'them.', '-LRB-', 'Thanks', '-RRB-', '.']
+expected_tokens == t.tokenize(s, convert_parentheses=True)
+expected_detoken = 'Good muffins cost $3.88 in New (York). Please (buy) me two of them. (Thanks).'
+expected_detoken == d.detokenize(t.tokenize(s, convert_parentheses=True), convert_parentheses=True)
+from nltk.tokenize.treebank import TreebankWordDetokenizer
+toks = ['hello', ',', 'i', 'ca', "n't", 'feel', 'my', 'feet', '!', 'Help', '!', '!']
+twd = TreebankWordDetokenizer()
+twd.detokenize(toks)
+toks = ['hello', ',', 'i', "can't", 'feel', ';', 'my', 'feet', '!', 'Help', '!', '!', 'He', 'said', ':', 'Help', ',', 'help', '?', '!']
+twd.detokenize(toks)
+from nltk.tokenize import TreebankWordTokenizer
+s = '''Good muffins cost $3.88\nin New York.  Please buy me\ntwo of them.\nThanks.'''
+TreebankWordTokenizer().tokenize(s)
+
+s = "They'll save and invest more."
+TreebankWordTokenizer().tokenize(s)
+
+s = "hi, my name can't hello,"
+TreebankWordTokenizer().tokenize(s)
+
