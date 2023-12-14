81f15b07e4b98e99ac246799e6d07e4b56ec1e89
Coder731 <stephenobrien1@gmail.com>
2023-12-14 13:28:15 +0000

    Add code to main py
    from
    
    nltk org

--- main.py
+++ main.py
@@ -5,3 +5,8 @@ tokens = nltk.word_tokenize(sentence)
 tokens
 tagged = nltk.pos_tag(tokens)
 tagged[0:6]
+entities = nltk.chunk.ne_chunk(tagged)
+# print(entities)
+from nltk.corpus import treebank
+t = treebank.parsed_sents('wsj_0001.mrg')[0]
+t.draw()
